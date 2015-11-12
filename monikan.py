355e14fcec77333fda94798365fb7c434e8fb364
chezmon <chezmonac@gmail.com>
2015-11-13 02:44:43 +0800

    Update monikan.py

--- monikan.py
+++ monikan.py
@@ -1,6 +1,6 @@
 # takes computer and player moves and uses if to see who wins!
 
 def outcome(com,pla):
-	if(com==pla):
+	if(com==pla): # both chose the same move, stalemate!
 		return "Stalemate"
-	elif(com)
+	elif(com) # if com chooses scissors, player chooses rock or paper, then what happens?
