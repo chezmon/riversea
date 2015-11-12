acbe0a4ea52e28223ef52832b7b353135311cb36
zeroomon <xiningchiang@gmail.com>
2015-11-13 02:10:11 +0800

    Update fish.py

--- fish.py
+++ fish.py
@@ -1,8 +1,22 @@
 # 
 # Fish game
-  random.choice( ['scissors', 'paper', 'stone'] )
-	Tail Swipe = scissors
-	Bite = paper
-	Guard = stone
-	com_o = random.choice
+random.choice( ['scissors', 'paper', 'stone'] )
+Tail Swipe = scissors
+Bite = paper
+Guard = stone
+com_o = random.choice
 	
+def game()
+	print ("Fight!")
+	who_wins(com_o,pla_o):
+		if com_o>pla_o:
+			print ("You are hit!")
+		elif com_o == pla_o:
+			print ("No one is hurt!")
+		else:
+			print ("You win!")
+			
+	def who_wins(scissors,paper,stone):
+		if scissors == scissors:
+			draw
+		elif scissors > paper:
